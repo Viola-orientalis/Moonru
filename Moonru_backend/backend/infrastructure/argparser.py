4d398dd6f2af1a5b2ce7da7d763d4b5b150c4523
Viola-orientalis <64053292+Viola-orientalis@users.noreply.github.com>
2025-10-29 16:08:46 +0900

    프로젝트 시작

--- Moonru_backend/backend/infrastructure/argparser.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-def parse_args(argv):
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--port", type=int, default=8000)
-    parser.add_argument("--env", type=str, default="dev")
-    return parser.parse_args(argv)
